215e13c5336fe55ae3c421b873f28da753ad4bf4
Soniabensaad <5687@holbertonstudents.com>
2022-12-21 18:19:25 +0100

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,37 +1,29 @@
 #include "sort.h"
 /**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
+ * bubble_sort -  list of integers
+ * @array: array
+ * @size: size of array
+ * Return: array of integers in ascending order by Bubble sort
  */
 void bubble_sort(int *array, size_t size)
 {
-    int swap;
-    size_t i, j;
-    if (!array || !size)
-    {
-        return;
-    }
-    
-    for ( i = 0; i < size - 1 ; i++)
-    {
-    
-        for (j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                swap = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = swap;
-                print_array(array, size);
-                
-            }
-           
-            
-            
-        }
-        
-
-    }
-    
+int swap;
+size_t i, j;
+if (!array || !size)
+{
+return;
+}
+for (i = 0; i < size - 1 ; i++)
+{
+for (j = 0; j < size - 1 - i; j++)
+{
+if (array[j] > array[j + 1])
+{
+swap = array[j];
+array[j] = array[j + 1];
+array[j + 1] = swap;
+print_array(array, size);
+}
+}
+}
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,28 +8,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, min;
-int swap;
-if (!array || !size)
-{
-return;
-}
-for (i = 0; i < size ; i++)
-{
-min = i;
-for (j = i + 1; j < size; j++)
-{
-if (array[j] < array[min])
-{
-min = j;
-}
-if (min != i)
-{
-swap = array[i];
-array[i] = array[min];
-array[min] = swap;
-print_array(array, size);
-}
-}
-}
+    size_t i, j, min;
+    int swap;
+    if (!array || !size)
+    {
+        return;
+    }
+    for (i = 0; i < size; i++)
+    {
+        min = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min])
+            {
+                min = j;
+            }
+            if (min != i)
+            {
+                swap = array[i];
+                array[i] = array[min];
+                array[min] = swap;
+                print_array(array, size);
+            }
+        }
+    }
 }
