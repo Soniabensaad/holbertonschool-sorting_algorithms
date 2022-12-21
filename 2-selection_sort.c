3ca34e8ebff69351d2bbdd45f0bc84368f46e708
Soniabensaad <5687@holbertonstudents.com>
2022-12-21 18:07:50 +0100

    slection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,37 +1,35 @@
 
 #include "sort.h"
 /**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
+ * selection_sort - array of integers in ascending order
+ *@array: array
+ *@size: size of array
+ *Retrun : array by selection_sort algorithm
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min ;
-    int swap;
-    if (!array || !size)
-    {
-        return;
-    }
-    
-    for (i = 0; i < size ; i++)
-    {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min])
-           {
-                min = j;
-            }
-            if (min != i)
-            {
-                swap = array[i];
-                array[i] = array[min];
-                array[min] = swap;
-                print_array(array, size);
-
-            }
-        }    
-    }
-
+size_t i, j, min;
+int swap;
+if (!array || !size)
+{
+return;
+}
+for (i = 0; i < size ; i++)
+{
+min = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min])
+{
+min = j;
+}
+if (min != i)
+{
+swap = array[i];
+array[i] = array[min];
+array[min] = swap;
+print_array(array, size);
+}
+}
+}
 }
