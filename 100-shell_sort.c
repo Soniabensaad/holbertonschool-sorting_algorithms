ae4b4713d95542e22dfd9b3e34dc70c3be92e1cc
Soniabensaad <5687@holbertonstudents.com>
2022-12-25 20:52:32 +0100

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,35 +1,35 @@
 #include "sort.h"
+/**
+ * shell_sort - an array of integers in ascending order
+ * @array: array
+ * @size: size
+ * Return : array
+*/
 void shell_sort(int *array, size_t size)
 {
-    size_t gap , i, j;
-    int tpr;
-   
-	if (!array || size < 2)
-    {
-		return;
-    }
-    gap = 1;
-    while (gap < size)
-		gap = (gap * 3) + 1;
-    gap = (gap -1) /3;
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            tpr = array[i];
-            for (j = i; j>= gap && array[j - gap] > tpr; j -=gap)
-            {
-                array[j] = array[j - gap];
-            }
-            array[j] = tpr;
-            
-
-        }
-        
-        gap = (gap - 1) / 3;
-        print_array(array, size);
-    }
-    
-    
-}   
+size_t gap, i, j;
+int tpr;
+if (!array || size < 2)
+{
+return;
+}
+gap = 1;
+while (gap < size)
+gap = (gap * 3) + 1;
+gap = (gap - 1) / 3;
+while (gap > 0)
+{
+for (i = gap; i < size; i++)
+{
+tpr = array[i];
+for (j = i; j >= gap && array[j - gap] > tpr; j -= gap)
+{
+array[j] = array[j - gap];
+}
+array[j] = tpr;
+}
+gap = (gap - 1) / 3;
+print_array(array, size);
+}
+}
 
