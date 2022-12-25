1188604ae84c1b9391de32b40d18281b7a5241d8
Soniabensaad <5687@holbertonstudents.com>
2022-12-25 20:37:36 +0100

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,21 +1,19 @@
 #include "sort.h"
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
+    size_t gap , i, j;
     int tpr;
    
 	if (!array || size < 2)
     {
 		return;
     }
-    while (gap < size / 3)
-		gap = gap * 3 + 1;
     while (gap > 0)
     {
         for (i = gap; i < size; i++)
         {
             tpr = array[i];
-            for (j = j; j>= gap && array[j - gap] > tpr; j = -gap)
+            for (j = i; j>= gap && array[j - gap] > tpr; j -=gap)
             {
                 array[j] = array[j - gap];
             }
@@ -24,7 +22,7 @@ void shell_sort(int *array, size_t size)
 
         }
         
-        gap /= 3;
+        gap = (gap * 3) + 1;
         print_array(array, size);
     }
     
