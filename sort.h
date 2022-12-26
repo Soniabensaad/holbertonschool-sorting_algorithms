dd3db90aaa10327771d7f64950afc9a1877bca46
Soniabensaad <5687@holbertonstudents.com>
2022-12-26 17:25:30 +0100

    merge sort

--- sort.h
+++ sort.h
@@ -25,4 +25,5 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 #endif
